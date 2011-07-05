fb43320a38cafeace185250d074cb4c107df3f22
Glenn Pratt <glennpratt@gmail.com>
2011-07-04 19:17:43 -0500

    Remove license cruft for now.

--- nfs/resources/export.rb
+++ nfs/resources/export.rb
@@ -1,23 +1,4 @@
-#
-# Cookbook Name:: apt
-# Resource:: repository
-#
-# Copyright 2010-2011, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-actions :add, :remove
+actions :create, :delete
 
 # Path to export, should be unique.
 # TODO - man exports says you can have one for each NFS type.
